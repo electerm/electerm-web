6054045b3a7c6ced4b56b373e2c5cab4f11a400c
ZHAO Xudong <zxdong@gmail.com>
2025-01-04 18:30:38 +0800

    Remove test

--- test/e2e/common/log.js
+++
@@ -1,3 +0,0 @@
-export default (...arg) => {
-  console.log('  >', ...arg)
-}
--- test/e2e/common/wait.js
+++
@@ -1,5 +0,0 @@
-/**
- * wait async
- * @param {*} time
- */
-export default time => new Promise(resolve => setTimeout(resolve, time))
