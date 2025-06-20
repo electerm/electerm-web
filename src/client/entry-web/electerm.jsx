8a90f1b62517601036a53f6c396fbb4eb9d5855a
ZHAO Xudong <zxdong@gmail.com>
2025-06-20 12:28:28 +0800

    Fix default font

--- src/client/entry-web/electerm.jsx
+++ src/client/entry-web/electerm.jsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import '../../../node_modules/antd/dist/reset.css'
 import '../../../node_modules/@xterm/xterm/css/xterm.css'
 import '../electerm-react/common/trzsz'
+import '@fontsource/maple-mono/index.css'
 import Main from '../web-components/web-main'
 
 const rootElement = document.getElementById('container')
