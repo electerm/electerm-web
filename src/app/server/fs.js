00051db2260d07239c9000eef4d3708491b1bba9
ZHAO Xudong <zxdong@gmail.com>
2026-04-18 17:08:05 +0800

    Remove log

--- src/app/server/fs.js
+++ src/app/server/fs.js
@@ -6,7 +6,6 @@ import { fsExport as fs } from '../lib/fs.js'
 
 export default function handleFs (ws, msg) {
   const { id, args, func } = msg
-  console.log('handleFs', id, args, func)
   fs[func](...args)
     .then(data => {
       ws.s({
